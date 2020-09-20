82076156441043429be679bdc8bd16d6608e52ea
barisbiyik <baris@lumbarsoft.com>
2020-09-20 12:22:58 +0300

    added dist

--- src/AddCard.vue
+++ src/AddCard.vue
@@ -6,7 +6,7 @@
       <div class="card offset-2 col-md-3">
         <div class="card-body tex-center d-flex align-items-center flex-column">
           <img height="128" class="img-responsive text-center mb-3"
-               :src="product.selectedImage == null ? '/src/assets/default.png' : product.selectedImage">
+               :src="product.selectedImage == null ? '/default.png' : product.selectedImage">
           <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
           <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Resim Seç</button>
         </div>
